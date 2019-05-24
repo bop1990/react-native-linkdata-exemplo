7dbc25f81ec508da376493321e6c0085c3439e64
Bruno de Oliveira Petry <bruno.petry@LINK-DATA.ASI>
2019-05-24 14:58:12 -0300

    apagando comentario no projeto android

--- android/app/src/main/java/com/projeto1/MainApplication.java
+++ android/app/src/main/java/com/projeto1/MainApplication.java
@@ -21,29 +21,6 @@ public class MainApplication extends NavigationApplication {
       protected String getJSMainModuleName() {
         return "index";
       }
-
-//      @Override
-//      protected List<ReactPackage> getPackages() {
-//        List<ReactPackage> listaSuper = super.getPackages();
-//        listaSuper.add(new SplashScreenReactPackage());
-//        return listaSuper;
-
-//        return Arrays.<ReactPackage>asList(
-//                (ReactPackage) listaSuper,
-//                new SplashScreenReactPackage()  //here
-//        );
-
-//        return Arrays.<ReactPackage>asList(
-//                (ReactPackage) super.getPackages(),
-//                new SplashScreenReactPackage()  //here
-//        );
-
-//          return Arrays.<ReactPackage>asList(
-//                  new MainReactPackage(),
-//                  new NavigationPackage(this),
-//                  new SplashScreenReactPackage()
-//          );
-//      }
     };
     return new ReactGateway(this, isDebug(), host);
   }
